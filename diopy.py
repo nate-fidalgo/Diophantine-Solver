3ea6119e3db42d29a7bc8a38a9be36b4286ee58c
nate-fidalgo <71114853+nate-fidalgo@users.noreply.github.com>
2021-09-16 19:55:28 -0400

    Add files via upload
    Update the solve to include the description of the diophantine equation when writting out dioequ.py
    Much better for quickly identifying what equation is what in dioequ.py

--- diopy.py
+++ diopy.py
@@ -1,49 +1,34 @@
 """
-
 README part ************************************************************************************************************************************************************************************
-
 A Python program to build a diophaintine equation solver program in python
 Eventually i am going to just read from a data file at least i think not sure yet. 
-
 Example of how you input into the command line and eventually from file if i do go thru with making this feature. (debating as is it really easiler to input in a file rather then terminal screen )
-
 x^2+z+y = 14  # diophaintine equation
 x z y         # variables 
 0 1 2         # start range of variables
 30 400 5000   # end range of variables
 1  10  100    # incremental jumps for variables
-
 Delimited  by a new line or a chosen delimiter for each set of equations in file
-
 Currently you just manually type the info into the commandline to build the dioequ.py file which contains the individual diophaintine equation programs delimited by $$$$$$$$$$$$$.
-
 Please feel free to modify anyway you want and improve on it.
-
 The reason why i wanted to write it is python has bignum built in if your like me in the olden days 
 you had problems just using standard data types because you can only go so high before overflows and other stuff causes problems.
-
 Now since bignum is built into this language and parsing is actually much easier as well it a simply way to code up what would take c, java ,...etc parsing a lot of lines of code more.
 Its more practical to cook up a program for distributive diophaintine equation solving using this small program.
-
 So the guys that have the more resources i like to see you run it on bunches of computers after all this generates programs that run in P time.
 Well P in the start and end ranges used and number of variables. Which seems for huge server rooms you see alot more neat stuff.
-
 Google and other places are better suited to solve hard polynomial time algorithms fast 
 Obviously NP is not going to work well on any server rooms but P will
-
 Exploring is the fun side to computational mathematics :)
 And i don't see many diophaintine solvers out there that are as general as this.
 I see how i can make the incremental jumps a little more general with lamda functions so then you really can search in any ranges and any patterns in those ranges for solutions.
 But for now this covers the 90% 
-
-
 ************************************************************************************************************************************************************************************************
-
 """
 
 
 equ = raw_input("Enter your diophaintine equation: \n") ; 
-
+equname = equ ;
 vars = raw_input( "Enter the distinct variable symbols used in diophaintine equation in left to right first occurence order: \n " ) ;
 varss = vars.split(" ") ;
 
@@ -86,7 +71,8 @@ varct = len(varss)
 print( str(varct) ) ;
 
 f.write("\n") ;
-
+f.write( "#### " + equname ) ;
+f.write("\n") ;
 
 for i in xrange(0,varct):
 	f.write( "i"+str(i)+" = "+ startrr[i]  +" ;\n" ) ;
